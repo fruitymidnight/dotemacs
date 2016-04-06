bcc6a7092c87c0e113a10798dd3bd875f671df82
Shin <shin@fruitymidnight.net>
2016-04-06 22:11:33 +0900

    git-gutter-fringe の色設定。少し落ち着いた色に変更

--- .emacs.d/config/packages/init-git-gutter-fringe.el
+++ .emacs.d/config/packages/init-git-gutter-fringe.el
@@ -3,4 +3,8 @@
 
 (when (require 'git-gutter-fringe nil t)
   (global-git-gutter-mode)
+  (set-face-foreground 'git-gutter-fr:modified "dark cyan")
+  (set-face-foreground 'git-gutter-fr:added    "forest green")
+  (set-face-foreground 'git-gutter-fr:deleted  "white")
   )
+
